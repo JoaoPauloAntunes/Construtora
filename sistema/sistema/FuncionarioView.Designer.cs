ee20182975e26ad39ca71f1163012e4ccf2957f5
JoaoPauloAntunes <jpantunesdesouza@gmail.com>
2020-10-01 23:59:55 -0300

    adicionado poo.dia; _

--- sistema/sistema/FuncionarioView.Designer.cs
+++ sistema/sistema/FuncionarioView.Designer.cs
@@ -37,7 +37,6 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "FuncionarioView";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.FuncionarioView_Load);
             this.ResumeLayout(false);
 
         }
